9fdb4225d074f33820f87a3bca1780e1d1ee40e3
feliph <feliphrenaud@gmail.com>
2023-10-16 19:52:53 -0300

    anotações de aulas e codigos

---
+++ separadorLetras/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String frase= "Hello new world";
+
+        for (int i=0; i <= frase.length() -2; i++){
+            System.out.print(frase.charAt(i) + " ");
+        }
+        //resposta esperada
+        //H e l l o   n e w   w o r l
+
+    }
+}
---
+++ whileAndLoops/src/Main.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //           SWITCH CASE
+
+//        //coletar o nome do produto
+//        System.out.println("Digite o nome do produto: ");
+//        Scanner s = new Scanner(System.in);
+//        String nomeProduto = s.next().toLowerCase();
+//
+//        //System.out.println(nomeProduto);
+//
+//        //Retornar o valor do produto
+//
+//        switch (nomeProduto) {
+//            case "banana":
+//                System.out.println("R$ 9,50");
+//                break;
+//            case "manga":
+//                System.out.println("R$ 12,80");
+//                break;
+//            case "abacate":
+//                System.out.println("R$ 16,90");
+//
+//        }
+
+        // FOR LOOP
+
+//        for (int i=1; i<=10; i++){
+//            System.out.println("Hello new world " + i);
+//
+//        }
+
+
+        //LER ITENS ARRAY UTILIZANDO FOR LOOP
+
+//        String[] cars = {"BMW", "Tesla", "Fiat","Ferrari"};
+//
+//        for (int i =0; i< cars.length; i++){
+//            System.out.println(cars[i]);
+//
+//        }
+
+        //          Nested Loops
+
+//        for(int i = 0; i < 2; i++){
+//            // esse loop deve retornar uma matriz por conter um for dentro de outro
+//            for( int j =0; j < 2; j++){
+//                System.out.println(i+ " " +j);
+//            }
+//        }
+
+        //         WHILE LOOP
+        int i = 1;
+        while (i <= 5){
+            System.out.println("Hello" + i);
+            i++;
+        }
+
+
+
+
+
+    }
+}
